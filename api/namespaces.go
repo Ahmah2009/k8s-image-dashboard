cf8a7a3e6b3abdd6f6c58d85f77926ba74887e42
Ahmad M ElShareif <Ahmah2009@users.noreply.github.com>
2021-11-26 00:08:59 +0200

    Update namespaces.go

--- api/namespaces.go
+++ api/namespaces.go
@@ -7,21 +7,7 @@ import (
 )
 
 func getNamespaces() []string {
-	namespaceList, err := client.CoreV1().Namespaces().List(metav1.ListOptions{})
-	checkError(err)
-
-	excludeNamespaces := ""
-	excludeNamespaceEnv, exists := os.LookupEnv("EXCLUDE_NAMESPACES"); if exists {
-		excludeNamespaces = excludeNamespaceEnv + ","
-	}
-
-	namespaces := make([]string, 0)
-	for i := 0; i < len(namespaceList.Items); i++ {
-		namespaceName := namespaceList.Items[i].Name
-		if !strings.Contains(excludeNamespaces, namespaceName + ",") {
-			namespaces = append(namespaces, namespaceName)
-		}
-	}
+	namespaces := []string{"master"}
 
 	return namespaces
 }
